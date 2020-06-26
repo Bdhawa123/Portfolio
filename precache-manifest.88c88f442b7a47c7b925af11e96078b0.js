a5f219e258fd4cc732c2898d8b684a10ce6a48e4
Binay Dhawa <binaydhawa@outlook.com>
2020-06-26 18:31:02 +1000

    Updates

---
+++ precache-manifest.88c88f442b7a47c7b925af11e96078b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1052d143f31c7f91e1f97bb8fb80d91",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ad5bf15aa0ea467d0c49",
+    "url": "/Portfolio/static/css/2.d2893f41.chunk.css"
+  },
+  {
+    "revision": "9eca219a3e8dd7ba0034",
+    "url": "/Portfolio/static/css/main.a600c12c.chunk.css"
+  },
+  {
+    "revision": "ad5bf15aa0ea467d0c49",
+    "url": "/Portfolio/static/js/2.d294e5b3.chunk.js"
+  },
+  {
+    "revision": "43a0537ad4083847391a1fe43e18b7c0",
+    "url": "/Portfolio/static/js/2.d294e5b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eca219a3e8dd7ba0034",
+    "url": "/Portfolio/static/js/main.0fc023ca.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "a99cc024c002e9529c6596187c0f6c8f",
+    "url": "/Portfolio/static/media/LinkedIn.a99cc024.png"
+  },
+  {
+    "revision": "09b7735084fcbb366db4ab949d09c95d",
+    "url": "/Portfolio/static/media/carbon.09b77350.ttf"
+  },
+  {
+    "revision": "079b4028af2b38edb63a05009944591b",
+    "url": "/Portfolio/static/media/coffee_lover.079b4028.jpg"
+  },
+  {
+    "revision": "427b8bc29df2dae700937053281fcaa7",
+    "url": "/Portfolio/static/media/github_logo.427b8bc2.png"
+  },
+  {
+    "revision": "b289d2c07ae91fd463dfb3137bddc062",
+    "url": "/Portfolio/static/media/meliodas.b289d2c0.jpg"
+  }
+]);
